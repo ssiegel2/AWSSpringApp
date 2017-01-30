6370cbfba90dd2bc9fd5e64e42e8aef00314dac6
Sam Siegel <ssiegel2@umbc.edu>
2017-01-30 14:46:01 -0500

    Implemented File Structure

---
+++ src/main/groovy/com/samsiegel/awsspringapp/AwsspringappApplication.groovy
@@ -0,0 +1,12 @@
+package com.samsiegel.awsspringapp
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class AwsspringappApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run AwsspringappApplication, args
+	}
+}
