490f04425c147b78e9f58b181325e99053fce9e0
Sam Siegel <ssiegel2@umbc.edu>
2017-02-03 00:29:15 -0500

    In progress: s3 upload

--- src/main/resources/static/js/s3Services.js
+++ src/main/resources/static/js/s3Services.js
@@ -1,44 +1,60 @@
-var BUCKET_NAME = 'awsspringapp';
-var API_VERSION = '2012-10-17';
+var uploadFile = function(name, file) {
 
-var poolData = {
-    UserPoolId: AWS_USER_POOL_ID,
-    ClientId: AWS_CLIENT_ID
-};
+    var BUCKET_NAME = 'awsspringapp';
+    var API_VERSION = '2012-10-17';
 
-var userPool = new AWSCognito.CognitoIdentityServiceProvider.CognitoUserPool(poolData);
-var currentUser = userPool.getCurrentUser();
+    var poolData = {
+        UserPoolId: AWS_USER_POOL_ID,
+        ClientId: AWS_CLIENT_ID
+    };
 
-if(currentUser != null) {
-    currentUser.getSession(function(err, session) {
+    var s3 = new AWS.S3({
+        apiVersion: API_VERSION,
+        params : {Bucket: BUCKET_NAME}
+    });
+
+    var userPool = new AWSCognito.CognitoIdentityServiceProvider.CognitoUserPool(poolData);
+    var currentUser = userPool.getCurrentUser();
+
+    if(currentUser != null) {
+        currentUser.getSession(function(err, session) {
+            if(err) {
+                console.log(err);
+                return;
+            }
+            console.log('session validity: ' + session.isValid());
+
+            AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+                IdentityPoolId : AWS_ID_POOL,
+                Logins : {
+                     'cognito-idp.us-east-1.amazonaws.com/us-east-1_94hEwNlgs' : session.idToken.jwtToken
+                }
+            });
+
+        });
+    }
+
+    s3.upload({
+        Key : name,
+        Body : file,
+        ACL : 'public-read'
+    }, function(err, data) {
         if(err) {
-            console.log(err);
+            console.log('File upload failed', err);
             return;
         }
-        console.log('session validity: ' + session.isValid());
-
-        AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-            IdentityPoolId : AWS_ID_POOL,
-            Logins : {
-                 'cognito-idp.us-east-1.amazonaws.com/us-east-1_94hEwNlgs' : session.idToken.jwtToken
-            }
-        });
-
-
-
-        var s3 = new AWS.S3({
-            apiVersion: API_VERSION,
-            params : {Bucket: BUCKET_NAME}
-        });
-
-        // Call S3 to list current buckets
-        s3.listObjects(function(err, data) {
-            if (err) {
-                console.log("Error", err);
-            } else {
-                console.log("Bucket List", data);
-            }
-        });
-
+        console.log('File upload successful');
     });
+
 }
+
+$(document).ready(function() {
+
+    $('#uploadSubmit').on('click', function(event){
+        event.preventDefault();
+        var file = $('#uploadInput')[0].files[0];
+        uploadFile(file.name, file);
+    });
+
+
+});
